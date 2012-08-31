10935d79d9874db02c70aece6dc62eedeea248b5
Donggu <donggu@media.mit.edu>
2012-08-31 17:02:57 -0400

    completed formWidget

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 //--------------------------------------------------------------
 int main(){
 	ofAppGlutWindow window; // create a window
+	window.setGlutDisplayString( "rgba double depth alpha samples");
 	// set width, height, mode (OF_WINDOW or OF_FULLSCREEN)
 	ofSetupOpenGL(&window, 1024, 768, OF_WINDOW);
 	ofRunApp(new realApp()); // start the app
--- src/realApp.cpp
+++ src/realApp.cpp
@@ -1,10 +1,12 @@
 #pragma warning(disable:4819)
+#include <cmath>
 #include "realApp.h"
-#include "formWidget.cpp"
+#include "formWidget.h"
+#include "gateWidget.h"
 
 
 //--------------------------------------------------------------
-ofxUICanvas *gui;
+formWidget *gui;
 
 ofxBox2dJoint j;
 void realApp::setup()
@@ -13,17 +15,6 @@ void realApp::setup()
 	ofBackgroundHex(0xfdefc2);
 	ofSetLogLevel(OF_LOG_NOTICE);
 	ofSetVerticalSync(true);
-
-	//gui
-	float xInit = OFX_UI_GLOBAL_WIDGET_SPACING; 
-    float length = 255-xInit; 
-
-    gui = new formWidget(length+xInit+2, 100, length+xInit, ofGetHeight()-200);
- 	ofAddListener(gui->newGUIEvent,this,&realApp::guiEvent);
-
-    gui->setDrawBack(true);
-    //gui->setDrawPadding(true);
-
 	
 	// Box2d
 	box2d.init();
@@ -36,54 +27,59 @@ void realApp::setup()
 	c.setPhysics(0.3, 0.3, 0.1);
 	c.setup(box2d.getWorld(), (ofGetWidth()/2)+ofRandom(-10, 10), 30, ofRandom(10, 20));
 	circles.push_back(c);
-//	widgets.push_back(c);
 
 	ofxBox2dCircle c2;
 	c2.setPhysics(0.3, 0.3, 0.1);
 	c2.setup(box2d.getWorld(), (ofGetWidth()/2)+ofRandom(-10, 10), 50, ofRandom(10, 20));
 	circles.push_back(c2);
-//	widgets.push_back(c2);
-
-	ofxBox2dRect b;
-	b.setPhysics(0.3,0.6,0.1);
-	b.setup(box2d.getWorld(), length+xInit+2,100,length+xInit,ofGetHeight()-200);
-	rects.push_back(b);
 
 	j.setup(box2d.getWorld(), c.body, c2.body);
 	j.setLength(100);
 	j.setDamping(0.2);
+
+
+	cout<<"hearing?" <<endl;
+	//gui
+	float xInit = OFX_UI_GLOBAL_WIDGET_SPACING; 
+    float length = 255-xInit; 
+	gui = new formWidget(length+xInit, (ofGetHeight()-200));
+ 	ofAddListener(gui->newGUIEvent,this,&realApp::guiEvent);
+	gui->setup(box2d.getWorld(),500,40);
+	gui->box.setVelocity(-10,5);
 }
 
 
 //--------------------------------------------------------------
 void realApp::update() {
-	
+	float angle=fmod(gui->box.getRotation(),360);
+	if(angle>180) angle-=360; else if(angle<-180) angle+=360;
+	if(angle>20 || angle<-20) gui->box.body->ApplyTorque(-angle*30.0f);
 	box2d.update();	
 }
 
 //--------------------------------------------------------------
 void realApp::draw() {
-
-	circles[0].draw();
-	circles[1].draw();
-
 	// some circles :)
-	for (int i=0; i<rects.size(); i++) {
-		ofFill();
-		ofSetHexColor(0xc0dd3b);
-		rects[i].draw();
-
-		ofVec2f pos=rects[i].getPosition();
-		float angle=rects[i].getRotation();
-		ofTranslate(pos);
-		ofRotate(angle);
-		gui->drawAtOrigin();
-		ofRotate(-angle);
-		ofTranslate(-pos);
+	ofFill();
+	ofSetHexColor(0xc0dd3b);
+	int i;
+	for(i=0;i<circles.size();++i)
+	{
+		circles[i].draw();
+		circles[i].addAttractionPoint(ofVec2f(200,300),1);
 	}
+	j.draw();
+	
+	gui->setColorBack(0x7777cc);
+	float angle=int(gui->box.getRotation()*10)/10.0f;
+	ofVec2f pos=gui->box.getPosition()-ofVec2f(gui->box.getWidth(),gui->box.getHeight()).getRotated(angle);	
+	ofTranslate(pos);
+	ofRotate(angle);
+	gui->draw();
+	ofRotate(-angle);
+	ofTranslate(-pos);
 
 	// some debug information
-
 	ofDrawBitmapString("Plug", 10, 15);
 }
 
@@ -144,6 +140,11 @@ void realApp::keyPressed(int key)
     {
         return;  
     }
+	ofxBox2dCircle c;
+	c.setPhysics(100, 0.3, 0.1);
+	c.setup(box2d.getWorld(), (ofGetWidth()/2)+ofRandom(-50, 50), ofRandom(0,50), ofRandom(10, 20));
+	c.setVelocity(ofRandom(-20,20),ofRandom(-20,20));
+	circles.push_back(c);
 	switch (key) 
 	{			
 		case 'f':
@@ -174,5 +175,5 @@ void realApp::gotMessage(ofMessage msg){
 
 ////--------------------------------------------------------------
 void realApp::dragEvent(ofDragInfo dragInfo){ 
-
+	
 }
